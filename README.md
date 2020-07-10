9b8cbdf0fdb259e2d708a1945a17854800d1cc59
andeeisaacs <andeefonder@gmail.com>
2020-07-09 19:38:37 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# background-hex
+# background-hex (A simple Javascript game.)
 A simple javascript, html and css project that changes the color of the background through a randomizer. 
+
+The randomizer appears on the screen and with a click anywhere on the screen, the 
+
+# User Stories
+
+As a user, when I visit the site, I am greeted with a hex color code in the middle of the screen.
+As a user, when I click anywhere on the screen, the hex color code changes randomly and the background changes to the color it currently is set to.
+
---
+++ javascript.js
@@ -0,0 +1,19 @@
+var color = "#eb4034"
+
+
+//Function that takes in the color variable as an argument and randomizes each hexidecimal index positions.
+    //there are SIX positions in the hexidecimal color code, made up of numbers 0-9 and letters a-f
+//split the string into an array.
+//iterate through the length of the array.
+//randomize the index between a integer or a letter
+//If its an integer, randomize between 0 and 9.
+//If its a letter, randomize between 0 and 5.
+    //can these previous two steps be joined? such as 10=a, 11=b, etc. 
+//Replace the index value with the randomized number or letter
+//Join the values back together into a string
+//return string to color variable.
+
+//select the div id and the style tag selector of the background
+//select the h1 element and replace the string with the color variable value. 
+//Replace the tag selector with color variable with an onclick.
+
