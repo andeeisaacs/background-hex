4559805420405fd0012c811c39ea1243164f4fc7
andeeisaacs <andeefonder@gmail.com>
2020-07-09 20:51:26 -0700

    Finish project

--- javascript.js
+++ javascript.js
@@ -3,17 +3,51 @@ var color = "#eb4034"
 
 //Function that takes in the color variable as an argument and randomizes each hexidecimal index positions.
     //there are SIX positions in the hexidecimal color code, made up of numbers 0-9 and letters a-f
-//split the string into an array.
-//iterate through the length of the array.
-//randomize the index between a integer or a letter
-//If its an integer, randomize between 0 and 9.
-//If its a letter, randomize between 0 and 5.
-    //can these previous two steps be joined? such as 10=a, 11=b, etc. 
-//Replace the index value with the randomized number or letter
-//Join the values back together into a string
-//return string to color variable.
 
-//select the div id and the style tag selector of the background
-//select the h1 element and replace the string with the color variable value. 
+const changeColor = () => {
+    //split the string into an array.
+    let newArr = color.split("")
+    let newColor = ["#"]
+    //iterate through the length of the array.
+    for(i = 1; i<newArr.length; i++){
+        //randomize the index value between a integer or a letter
+        newArr[i] = Math.floor(Math.random() * 15)
+        //if below 9, just return value of the integer
+        if(newArr[i] <= 9) {
+            newColor.push(newArr[i])
+            //If its greater than 9, set a letter value
+        } else if(newArr[i] === 10){
+            newColor.push("a")       
+        } else if(newArr[i] === 11){
+            newColor.push("b")
+        } else if(newArr[i] === 12){
+            newColor.push("c")
+        } else if(newArr[i] === 13){
+            newColor.push("d")
+        } else if(newArr[i] === 14){
+            newColor.push("e")
+        } else {
+            newColor.push("f")
+        }
+    }
+    //Join the values back together into a string and re-assign color
+    //return string to color variable.
+    color = newColor.join("").toUpperCase()
+    return changeHTML(color)
+}
+
+
 //Replace the tag selector with color variable with an onclick.
+const changeHTML = (color) => {
+    console.log(color)
+    //select the div id and the style tag selector of the background color
+    document.getElementById('colorbox').style.backgroundColor = color;
+    //select the h1 element span and replace the string with the color variable value. 
+    document.getElementById('hexcolor').innerHTML = color;
+
+}
+
+// changeColor()
+
+
 
